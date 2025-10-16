c8f01c225b9252d13fdd55f351c473f0f3b7d61d
Jonghyo Lee <na1307@outlook.kr>
2025-10-17 00:23:06 +0900

    Pop

--- BluehillNuGetPages/Program.cs
+++ BluehillNuGetPages/Program.cs
@@ -1,11 +1,19 @@
-using BluehillNuGetPages;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace BluehillNuGetPages;
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+internal static class Program {
+    private static Task Main(string[] args) {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-await builder.Build().RunAsync();
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(_ => new HttpClient {
+            BaseAddress = new(builder.HostEnvironment.BaseAddress)
+        });
+
+        return builder.Build().RunAsync();
+    }
+}
