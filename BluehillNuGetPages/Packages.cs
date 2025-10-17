5ee7e03e0c5ce79af4eda8fece4a6dc8e91d38c3
Jonghyo Lee <na1307@outlook.kr>
2025-10-17 10:16:12 +0900

    Add experimental code execution page for interactive package demos

---
+++ BluehillNuGetPages/Packages.cs
@@ -0,0 +1,11 @@
+﻿namespace BluehillNuGetPages;
+
+internal static class Packages {
+    public static readonly Uri Hangul = new("https://hangul.bluehill.dev");
+    public static readonly Uri Deconstructors = new("https://nuget.bluehill.dev/Bluehill.Deconstructors/");
+    public static readonly Uri Analyzers = new("https://analyzers.bluehill.dev");
+    public static readonly Uri CompilerServices = new("https://nuget.bluehill.dev/Bluehill.CompilerServices/");
+    public static readonly Uri NativeCom = new("https://github.com/BluehillNuGet/Bluehill.NativeCom");
+    public static readonly Uri StringExtensions = new("https://github.com/BluehillNuGet/Bluehill.StringExtensions");
+    public static readonly Uri SdkStyleTemplates = new("https://github.com/BluehillNuGet/Bluehill.SdkStyleTemplates");
+}
